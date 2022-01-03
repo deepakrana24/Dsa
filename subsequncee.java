39e1d449de8c38778aa05ebefed1d784ba4f42c7
DEEPAK RANA <hypermac@DEEPAKs-MacBook-Pro.local>
2022-01-03 20:59:55 +0530

    new  code for quenstions

---
+++ subsequncee.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+// printing all sequences 
+
+
+/**
+ * subsequncee
+ */
+public class subsequncee {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        printss(str , " " );
+        scn.close();
+    }
+
+    private static void printss(String str, String string) {
+    }
+}
